507a1e3e4c05608a43332eb34f637cb96ff95480
javier1890 <francisco.uribe@r9.cl>
2020-02-14 13:55:10 -0300

    ultimo cambio de la clase del dia vernes

--- Menu.java
+++ Menu.java
@@ -10,6 +10,7 @@ public class Menu {
 		Scanner a = new Scanner(System.in);
 		int op1, op2, op3, op4;
 		boolean continuar = false;
+		final String clave="root";
 
 		while (!continuar) {
 			System.out.println("|	**************Menu principal**************	|");
@@ -18,32 +19,41 @@ public class Menu {
 			System.out.println("	3.-Salir");
 			op1 = a.nextInt();
 			do {
+
 				switch (op1) {
 				case 1:
-					System.out.println("|	**************Menu Administrador**************	|");
+					// System.out.println("Ingresar contraseña de administrador");
+					clave = a.nextInt();
 
-					System.out.println("	1.-Crear cliente");
-
-					System.out.println("	2.-Mostrar cliente");
-
-					System.out.println("    3.-salir           ");
-					op2 = a.nextInt();
-					do {
-
-						switch (op2) {
-						case 1:
-							System.out.println("creando cliente");
-							break;
-						case 2:
-							System.out.println("mostrar cliente");
-							break;
-						case 3:
-							continuar = true;
-							System.out.println(" adios");
-							break;
-						}
+					if (clave != 12345) {
 						
-					} while (op1 < 1 && op1 > 3);
+					}
+					else {
+						System.out.println("|	**************Menu Administrador**************	|");
+
+						System.out.println("	1.-Crear cliente");
+
+						System.out.println("	2.-Mostrar cliente");
+
+						System.out.println("    3.-salir           ");
+						op2 = a.nextInt();
+						do {
+
+							switch (op2) {
+							case 1:
+								System.out.println("creando cliente");
+								break;
+							case 2:
+								System.out.println("mostrar cliente");
+								break;
+							case 3:
+								// continuar = true;
+								System.out.println(" adios");
+								break;
+							}
+
+						} while (op1 < 1 && op1 > 3);
+					}
 
 					break;
 				case 2:
